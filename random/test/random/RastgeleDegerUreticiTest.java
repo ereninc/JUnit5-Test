c0f1294d4fb4eea899fdc8ee1af882d355ffa5be
Eren Inanci <erencan_inanci@hotmail.com>
2021-04-02 02:24:22 +0300

    JUnit API'lar yerleştirildi.

--- random/test/random/RastgeleDegerUreticiTest.java
+++ random/test/random/RastgeleDegerUreticiTest.java
@@ -1,5 +1,6 @@
 /**
- * 
+ * @author ASUS 02.04.2021 - 01:42
+ *
  */
 package random;
 
@@ -9,20 +10,40 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-/**
- * @author ASUS 02.04.2021 - 01:42
- *
- */
+
 class RastgeleDegerUreticiTest {
 	
+	RastgeleDegerUretici rastgele;
+	@BeforeAll
+	public static void setupAll() {
+		//awake methodu gibi ilk çalýþýyor
+	}
+	
+	@BeforeEach
+	public void setup() {
+		//Her test birimi öncesinde burasý çalýþýyor.
+		rastgele = new RastgeleDegerUretici();
+	}
+	
+	//Elimize rastgele deðer geçmiþ mi? Yani 0'dan farklý bir deðer var mý kontrol ediliyor.
 	@Test
-	void test() {
-		RastgeleDegerUretici rastgele = new RastgeleDegerUretici();
+	@DisplayName("Rastgele Üretilen Deðer 0'dan Büyük Mü?")
+	void RastgeleDegerSifirdanBuyukMuTest() {
 		long deger = rastgele.rastgeleUretici();
 		long uretilenDeger = deger>0? deger:0;
 		assertEquals(uretilenDeger, deger);
 	}
-
+	
+	@AfterEach
+	public void tearDown() {
+		//Her test biriminden sonra burasý çalýþýyor.
+	}
+	
+	@AfterAll
+	public void tearDownAll() {
+		//Bütün testler bittiðinde burasý çalýþýyor.
+	}
 }
